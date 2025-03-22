cb1658e77b5d7b5c27cd52e09d4b7c6cb51de700
Morgan Koh <delacrixmorgan@pm.me>
2025-03-22 11:45:46 +0100

    Convert build.gradle to KTS and TOML

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+    alias(libs.plugins.intelliJ)
+    alias(libs.plugins.kotlin)
+}
+
+version = "2025.1"
+
+intellij {
+    version.set(libs.versions.intelliJ.version.get())
+    type.set("IC")
+    plugins.set(
+        listOf(
+            "org.jetbrains.android:${libs.versions.android.plugin.get()}",
+            "java",
+            "platform-images"
+        )
+    ) // Specify Android plugin with explicit version
+    updateSinceUntilBuild.set(false)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.commons.io)
+    implementation(libs.commons.math3)
+    implementation(libs.commons.lang3)
+    implementation(libs.imgscalr.lib)
+    implementation(libs.thumbnailator)
+    implementation(libs.gson)
+}
